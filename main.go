137a040fe8bec10978e1b17a585b1f20900168e3
muhammadharis <muhammadharis1010@gmail.com>
2019-09-25 21:30:09 -0400

    Set up the ProduceAPIServer on GRPC

--- main.go
+++ main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	//"net"
+	//"time"
+
+	broker "github.com/muhammadharis/grpc/protos/broker"
+	brokerImpl "github.com/muhammadharis/grpc/services/broker"
+	redis "github.com/go-redis/redis"
+
+	"google.golang.org/grpc"
 )
 
 func main() {
-	fmt.Println("hi")
+	server := grpc.NewServer()
+	broker.RegisterProduceAPIServer(server, &brokerImpl.BrokerImpl{})
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	client.LPush("key", "a", "b", "c", "d")
+	
+	fmt.Println(client.LRange("key", 0, -1))
+
+	client.Set("key1", "52", 0)
+	val, err := client.Get("key1").Int64()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(val+1)
 }
