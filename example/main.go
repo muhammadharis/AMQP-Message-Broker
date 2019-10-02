dd3da78296e7fe05658c5f5927d5bcd4eb9a7364
muhammadharis <muhammadharis1010@gmail.com>
2019-10-02 00:56:34 -0400

    Modularized code into helper functions

--- example/main.go
+++ example/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+
+	//broker "github.com/muhammadharis/grpc/protos/broker"
 )
 
 func main() {
 	argsWithoutProg := os.Args[1:]
-	if argsWithoutProg[0] == "0" {
+	if argsWithoutProg[0] == "publish" {
 		fmt.Println("0")
 		Message()
-	} else if argsWithoutProg[0] == "1" {
+	} else if argsWithoutProg[0] == "subscribe" {
 		fmt.Println("1")
 		Read()
 	}
--- example/producer.go
+++ example/producer.go
@@ -4,44 +4,80 @@ import (
 	"context"
 
 	broker "github.com/muhammadharis/grpc/protos/broker"
+	"google.golang.org/grpc"
 )
 
-// Message allows a producer to produce a fanout message to the broker
-func Message() error {
+func openConnection(host string) (*grpc.ClientConn, error) {
 	conn, err := CreateGrpcClientConnection("localhost:8080")
 	if err != nil {
-		return err
+		return nil, err
 	}
-	
+	return conn, nil
+}
+
+// InitializeProducerClient initializes the Produce API client
+func InitializeProducerClient(host string) (broker.ProduceAPIClient){
+	conn, err := openConnection(host)
+	if err != nil {
+		panic(err)
+	}
 	// Initialize the client
 	client := broker.NewProduceAPIClient(conn)
+	return client
+}
 
-	// Create a new exchange
+// CreateExchange creates a new exchange with a specified name
+func CreateExchange(client broker.ProduceAPIClient, exchangeName, exchangeType string) {
 	createExchangeRequest := &broker.CreateExchangeRequest {
-		ExchangeName : "my_exchange",
-  		Type: "fanout",
+		ExchangeName : exchangeName,
+		Type: exchangeType,
 	}
 	client.CreateExchange(context.Background(), createExchangeRequest)
+}
 
-	// Create a new queue
+// CreateQueue creates a new Queue with a specified name
+func CreateQueue(client broker.ProduceAPIClient, queueName string) {
 	createQueueRequest := &broker.CreateQueueRequest {
-		QueueName: "my_queue",
+		QueueName: queueName,
 	}
 	client.CreateQueue(context.Background(), createQueueRequest)
+}
 
-	// Create a binding between queue and exchange
+// CreateBinding binds an existing queue to an existing exchange
+func CreateBinding(client broker.ProduceAPIClient, exchangeName, queueName string) {
 	bindingRequest := &broker.BindQueueRequest {
-		ExchangeName: "my_exchange",
-		QueueName: "my_queue",
+		ExchangeName: exchangeName,
+		QueueName: queueName,
 	}
 	client.BindQueue(context.Background(), bindingRequest)
+}
 
-	// Produce a direct message to my_queue
+// ProduceMessage sends a message to the correct queue and exchange
+func ProduceMessage(client broker.ProduceAPIClient, exchangeName, queueName string, messageSet [][]byte){
 	produceRequest := &broker.ProduceRequest {
-		ExchangeName: "my_exchange",
-		QueueName: "my_queue",
+		ExchangeName: exchangeName,
+		QueueName: queueName,
 		MessageSet: [][]byte{[]byte("Hello, world!"), []byte("This is a message")},
 	}
 	client.ProduceMessage(context.Background(), produceRequest)
+}
+
+// Message allows a producer to produce a fanout message to the broker
+func Message() error {
+	// Initialize the client
+	client := InitializeProducerClient("localhost:8080")
+
+	// Create a new exchange
+	CreateExchange(client, "my_exchange", "fanout")
+
+	// Create a new queue
+	CreateQueue(client, "my_queue")
+
+	// Create a binding between queue and exchange
+	CreateBinding(client, "my_exchange", "my_queue")
+
+	// Produce a direct message to my_queue
+	ProduceMessage(client, "my_exchange", "my_queue", [][]byte{[]byte("Hello, world!"), []byte("This is a message")})
+	
 	return nil
 }
