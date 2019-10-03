0375635afc8cd7810e03fc03c507059f087b92e9
muhammadharis <muhammadharis1010@gmail.com>
2019-10-02 21:33:21 -0400

    Modularized consumer example into helper functions and changed Make target name

--- example/consumer.go
+++ example/consumer.go
@@ -8,23 +8,38 @@ import (
 	broker "github.com/muhammadharis/AMQP-Message-Broker/protos/broker"
 )
 
-// Read allows a consumer to consume a message from the queue
-func Read() error {
-	conn, err := CreateGrpcClientConnection("localhost:8080")
+// InitializeConsumerClient initializes the Consumer API client
+func InitializeConsumerClient(host string) (broker.ConsumerAPIClient){
+	conn, err := openConnection(host)
 	if err != nil {
-		return err
+		panic(err)
 	}
+	// Initialize the client
 	client := broker.NewConsumerAPIClient(conn)
+	return client
+}
 
+// CreateReadStream opens a stream that the consumer can read messages from
+func CreateReadStream(client broker.ConsumerAPIClient) (broker.ConsumerAPI_ReadMessageClient, error) {
 	consumerRequest := &broker.ConsumerReadRequest{
 		QueueName: "my_queue",
 	}
-	
-	stream, err := client.ReadMessage(context.Background(), consumerRequest)
+	return client.ReadMessage(context.Background(), consumerRequest)
+}
+
+// Read allows a consumer to consume a message from the queue
+func Read() error {
+
+	// Initialize client
+	client := InitializeConsumerClient("localhost:8080")
+
+	// Create a reading stream
+	stream, err := CreateReadStream(client)
 	if err != nil {
 		return err
 	}
 
+	// Receive messages on the stream
 	for {
 		msg, err := stream.Recv()
 		if err == io.EOF {
@@ -33,7 +48,7 @@ func Read() error {
 		if err != nil {
 			return err
 		}
-		fmt.Println(msg.GetMessage())
+		fmt.Println(msg.GetMessage()) //Print the messages
 	}
 }
 
